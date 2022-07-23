3b4a4169f611a82876829c10e0c6c1d12e760bd3
Kevin Oliveira <kevin@meliuz.com.br>
2022-07-23 09:04:14 -0300

    fix: Update event only if needed based on summary

--- app/modules/event/commands/update.py
+++ app/modules/event/commands/update.py
@@ -1,5 +1,6 @@
 from app.core.firestore import db
 from app.core.google_calendar import GoogleCalendar
+from app.modules.event.commands.find import FindEventCommand
 from app.modules.match.models.match import Match
 
 
@@ -9,6 +10,11 @@ class UpdateEventCommand:
         self.event_id = event_id
 
     def execute(self):
+        event = FindEventCommand(match=self.match).execute()
+
+        if event['match']['summary'] == self.match.summary:
+            return
+
         GoogleCalendar().update_event(match=self.match, event_id=self.event_id)
 
         data = {'google_calendar_event_id': self.event_id, 'match': self.match.dict()}
