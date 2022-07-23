4d9a2a73f1513c5876972498e0e3fe3f7d68cd36
Kevin Oliveira <kevin@meliuz.com.br>
2022-07-23 11:12:12 -0300

    feat: Add lambda_function with default code to get events

---
+++ lambda_function.py
@@ -0,0 +1,33 @@
+from app.modules.event.commands.create import CreateEventCommand
+from app.modules.event.commands.find import FindEventCommand
+from app.modules.event.commands.update import UpdateEventCommand
+from app.modules.match.commands.list.asia import ListAsiaMatches
+from app.modules.match.commands.list.br import ListBRMatches
+from app.modules.match.commands.list.eu import ListEUMatches
+from app.modules.match.commands.list.latam import ListLatamMatches
+from app.modules.match.commands.list.mundial import ListMundialMatches
+from app.modules.match.commands.list.na import ListNAMatches
+
+
+def lambda_handler(event, context):   
+    matches = []
+
+    matches.extend(ListAsiaMatches().execute())
+    matches.extend(ListBRMatches().execute())
+    matches.extend(ListEUMatches().execute())
+    matches.extend(ListLatamMatches().execute())
+    matches.extend(ListMundialMatches().execute())
+    matches.extend(ListNAMatches().execute())
+
+    print(f'{len(matches)} matches found')
+
+    for match in matches:
+        event = FindEventCommand(match=match).execute()
+
+        if event:
+            print(f'Match {match.summary} event already created, updating if needed...')
+            UpdateEventCommand(match=match, event_id=event['google_calendar_event_id']).execute()
+            continue
+
+        print(f'Creating event for match {match.summary} ...')
+        CreateEventCommand(match=match).execute()
